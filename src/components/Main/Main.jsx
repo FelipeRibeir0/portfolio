7ee697ba2fbc94ab20fd872e2302d6fe2af1dbde
FelipeRibeir0 <fecribeiro2003@gmail.com>
2025-04-06 16:47:16 -0300

    refactor: description

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,10 +6,9 @@ export const Main = () => {
     return <main className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Felipe</h1>
-            <p className={styles.description}>I'm a fullstack developer with academic experience and personal projects in backend, frontend, and interface design.
-                <p>I enjoy understanding how things work end to end and aim to build solutions that are functional, well-structured, and easy to use.</p>
-                <p>Reach out if you'd like to learn more!</p>
-            </p>
+            <p className={styles.description}>I'm a fullstack developer with academic experience and personal projects in backend, frontend, and interface design.</p>
+            <p className={styles.description} >I enjoy understanding how things work end to end and aim to build solutions that are functional, well-structured, and easy to use.</p>
+            <p className={styles.description}>Reach out if you'd like to learn more!</p>
             <a href="mailto:felipecorreiaribeiro7@gmail.com" className={styles.contactBtn}>Contact Me</a>
         </div>
         <img src={getImageUrl("main/mainImage.png")} alt="Image of me" className={styles.heroImg} />
