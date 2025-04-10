865f4a2b27ad374b1727ef85dd19e29a22ec1bd6
FelipeRibeir0 <fecribeiro2003@gmail.com>
2025-04-10 19:32:11 -0300

    refactor: conditional rendering

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,23 +1,36 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
+import styles from "./ProjectCard.module.css";
 
-import styles from "./ProjectCard.module.css"
-
-export const ProjectCard = ({ project: {title, imageSrc, description, skills, demo, source} }) => {
-    return <div className={styles.container}>
-        <img src={getImageUrl(imageSrc)}
-            alt={`Image of ${title}`} 
-            className={styles.image}/>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.description}>{description}</p>
-        <ul className={styles.skills}>
-            {skills.map((skill, id) => {
-                return <li key={id} className={styles.skill}>{skill}</li>
-            })}
-        </ul>
-        <div className={styles.links}>
-            <a href={demo} className={styles.link}>Demo</a>
-            <a href={source} className={styles.link}>Source</a>
+export const ProjectCard = ({ project: { title, imageSrc, description, skills, demo, source } }) => {
+    return (
+        <div className={styles.container}>
+            <img
+                src={getImageUrl(imageSrc)}
+                alt={`Image of ${title}`}
+                className={styles.image}
+            />
+            <h3 className={styles.title}>{title}</h3>
+            <p className={styles.description}>{description}</p>
+            <ul className={styles.skills}>
+                {skills.map((skill, id) => (
+                    <li key={id} className={styles.skill}>
+                        {skill}
+                    </li>
+                ))}
+            </ul>
+            <div className={styles.links}>
+                {demo && (
+                    <a href={demo} className={styles.link} target="_blank" rel="noopener noreferrer">
+                        Demo
+                    </a>
+                )}
+                {source && (
+                    <a href={source} className={styles.link} target="_blank" rel="noopener noreferrer">
+                        Source
+                    </a>
+                )}
+            </div>
         </div>
-    </div>
-}
+    );
+};
