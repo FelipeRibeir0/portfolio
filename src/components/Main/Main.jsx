738d92297cc4cc2045fefa4ab2c8bcd883b0db22
FelipeRibeir0 <fecribeiro2003@gmail.com>
2025-04-14 20:21:10 -0300

    add: curriculum button

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ import { getImageUrl } from "../../utils";
 
 export const Main = () => {
     const { t } = useTranslation('main');
-    
+
     return (
         <main className={styles.container}>
             <div className={styles.content}>
@@ -13,17 +13,19 @@ export const Main = () => {
                 <p className={styles.description}>{t("description1")}</p>
                 <p className={styles.description}>{t("description2")}</p>
                 <p className={styles.description}>{t("description3")}</p>
-                <a 
-                    href="mailto:felipecorreiaribeiro7@gmail.com" 
+                <a
+                    href="https://drive.google.com/file/d/1cqzl2mauhYweG1saZFomEcOlibDuV5CY/view?usp=drive_link"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className={styles.contactBtn}
                 >
-                    {t("contactBtn")}
+                    {t("curriculumBtn")}
                 </a>
             </div>
-            <img 
-                src={getImageUrl("main/mainImage.png")} 
-                alt="Image of me" 
-                className={styles.heroImg} 
+            <img
+                src={getImageUrl("main/mainImage.png")}
+                alt="Image of me"
+                className={styles.heroImg}
             />
             <div className={styles.topBlur}></div>
             <div className={styles.bottomBlur}></div>
